eeaa6c0ee0edfc64c9379cc470f962c642b99d0a
1Khizar <khizar12006@gmail.com>
2025-06-24 13:09:48 +0500

    first commit

---
+++ Day_1_Task.py
@@ -0,0 +1,9 @@
+# Taking user inputs
+int_input = int(input("Enter the integer : "))
+float_input = float(input("Enter the float value : "))
+string_input = input("Enter a string : ")
+
+# Now Displaying the inputs using formatted print statements
+print(f"\nInteger Value: {int_input}")
+print(f"\nFloat Value: {float_input}")
+print(f"\nString Value: '{string_input}'")
